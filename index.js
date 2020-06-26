ce0c667ea2f2e6ab2360b8e5ba4606883f51871d
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-27 00:28:35 +0300

    created helper instructions and error handling for files not found

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
-console.log('It was executed');
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
+
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of file to execute')
+  .action(async ({ filename }) => {
+      //filename = args.filename
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+    const start = debounce (() => {
+      console.log('STARTING PROGRAM');
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+  program.parse(process.argv)
